722b9cc569bcfb1e842825b9e4790e3c14e9edd7
JairRamosG <ramosgonzalezjair@gmail.com>
2026-05-18 17:10:30 -0600

    mostrar mapa vacío

--- src/app.py
+++ src/app.py
@@ -29,3 +29,26 @@ with col1:
             st.session_state.df = df 
             st.session_state.last_updated = datetime.now().strftime("%d %b %Y, %H:%M")
         st.success('Listo')
+
+with col2:
+    st.markdown(f'Ultima actualización - {st.session_state.last_updated}')
+
+if st.session_state.df is not None:
+    df = st.session_state.df
+
+    c1, c2, c3 = st.columns(3)
+    c1.metric('Total de estaciones', len(df))
+
+    lat,lon = df.iloc[0][['latitud', 'longitud']]
+    m = folium.Map(location = [lat, lon], zoom_start = 12)
+    fg = folium.FeatureGroup(name = "Estaciones Londres")
+
+    st_folium(
+        m,
+        feature_group_to_add = fg,
+        width = "100%",
+        height = 600,
+    )
+
+else:
+    st.info('Sin datos actualizados')
