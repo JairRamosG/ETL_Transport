68debf38d2c5ccfd7cad859165e97222d8218189
JairRamosG <ramosgonzalezjair@gmail.com>
2026-05-18 17:31:35 -0600

    corrección de colores

--- src/app.py
+++ src/app.py
@@ -43,11 +43,58 @@ if st.session_state.df is not None:
     m = folium.Map(location = [lat, lon], zoom_start = 12)
     fg = folium.FeatureGroup(name = "Estaciones Londres")
 
+
+    # Iteración por cada estación
+    for estacion in df.itertuples(index = False):
+        nombre = estacion.nombre_estacion
+        lat, lon = estacion.latitud, estacion.longitud
+        espacios = estacion.nEspaciosDisp
+        bicis_estandar = estacion.nBicisEstandar
+        bicis_e = estacion.nBicisElectricas
+
+        total_bicis = bicis_estandar + bicis_e
+        # ponerle colores
+        if total_bicis == 0:
+            bg_color = "#a82424"
+            color = "red"
+        elif total_bicis <= 7:
+            bg_color = "#c0e04d"
+            color = "orange"   
+        else:
+            bg_color = "#88f09b"
+            color = "green"
+            
+        # pupop de cada estación
+        popup_str = f"""
+        <div style="
+            background-color:{bg_color};
+            border-radius:8px;
+            padding:8px 12px;
+            font-size:13px;
+            line-height:1.5;
+            color:#333;
+        ">
+            <b style="font-size:14px;">{nombre}</b><br>
+            🚲 {bicis_estandar} estándar<br>
+            ⚡ {bicis_e} eléctricas<br>
+            🅿️ {espacios} espacios
+        </div> """
+
+        # poner figurita en el mapa
+        fg.add_child(
+            folium.Marker(
+                location = [lat, lon],
+                popup = folium.Popup(popup_str, max_width = 250),
+                tool_tip = nombre,
+                icon = folium.Icon(color = color)
+            )
+        )
+
     st_folium(
         m,
         feature_group_to_add = fg,
         width = "100%",
-        height = 600,
+        height = 700,
     )
 
 else:
